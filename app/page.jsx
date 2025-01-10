361dc93453786b863e428a42a1f4db15076455c6
Splendorg <splendorg@protonmail.com>
2025-01-10 19:14:31 +0300

    explore section

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
 import Hero from '@/sections/Hero';
 import Navbar from '@/components/Navbar';
 import '@/styles/globals.css'
+import About from '@/sections/About';
+import Explore from '@/sections/Explore';
 export default function Home() {
   return (
     <div className='bg-primary-black overflow-hidden'>
       <Navbar />
       <Hero />
+      <div className='relative'>
+        <About />
+        <div className='gradient-03 z-0' />
+        <Explore />
+      </div>
 
     </div>
 
